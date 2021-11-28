1cfc66c085aba5415238f1d65bac350997fc3133
ShivaniS00 <94316703+ShivaniS00@users.noreply.github.com>
2021-11-28 17:19:56 +0530

    Binary tree
    Binary tree

---
+++ Binary 0 and 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+Void main() 
+{
+int i,j;
+for(i=0;i<6;i++) 
+{
+for(j=0;j<i;j++) 
+{
+Printf("%d %d\n",i, j) ;
+}
+}
+if((i+j%2)=0) 
+{
+Printf("0") ;
+else
+{
+Printf("1) ;
+}
+}
+}
